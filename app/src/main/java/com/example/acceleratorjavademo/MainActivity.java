3f15dc335d47e6cc0c8bd27923ba30ff0a83ba59
Alexey Gerasimov <joahan.hax@gmail.com>
2019-03-24 22:06:24 +0800

    adding playground reading and displaying sensor data x/y/z direction

---
+++ app/src/main/java/com/example/acceleratorjavademo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.acceleratorjavademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private TextView xText, yText, zText;
+    private Sensor positionSensor;
+    private SensorManager SM;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create Sensor Manager
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        //Accelerometer sensor
+        positionSensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Register Sensor Listener
+        SM.registerListener(this, positionSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //Assign Text Views
+        xText = (TextView)findViewById(R.id.xText);
+        yText = (TextView)findViewById(R.id.yText);
+        zText = (TextView)findViewById(R.id.zText);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        xText.setText("X: " + event.values[0]);
+        yText.setText("Y: " + event.values[1]);
+        zText.setText("Z: " + event.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        //Not in use
+    }
+}
